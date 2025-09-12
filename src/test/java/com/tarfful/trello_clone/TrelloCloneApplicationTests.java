b5178239a7b7b251fb3f995122bc4b7dc6bfb1fd
DAHUJT <beyonrus2003@gmail.com>
2025-09-12 16:06:20 +0400

    Initial commit: Spring Boot project structure with required dependencies

---
+++ src/test/java/com/tarfful/trello_clone/TrelloCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarfful.trello_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
